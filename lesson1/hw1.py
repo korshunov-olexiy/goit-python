5d0df9b2808f304da5a7841dcdf117d764dbf8ee
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-08-31 14:47:12 +0300

    main

---
+++ lesson1/hw1.py
@@ -0,0 +1,7 @@
+first_house_length = 50
+first_house_height = 3.5
+second_house_length = 60
+second_house_height = 4
+total_tree_count = first_house_height/0.25*first_house_length/2//3+second_house_height/0.25*second_house_length/2//3
+
+print(total_tree_count)
