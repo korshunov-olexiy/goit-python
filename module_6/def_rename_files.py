4b8baab5cedc37712c99bc80c9f5bdb8dc3dbfa2
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-10-05 16:22:35 +0300

    main

--- module_6/def_rename_files.py
+++ module_6/def_rename_files.py
@@ -9,13 +9,17 @@ documents_ext = ['doc', 'docx', 'txt']
 music_ext = ['mp3', 'ogg', 'wav', 'amr']
 archives_ext = ['zip', 'gz', 'tar']
 
-pictures = {'name': 'Изображения', 'dir_name': 'images', 'ext': pictures_ext, 'files': []}
-movies = {'name': 'Видео', 'dir_name': 'video', 'ext': movies_ext, 'files': []}
-documents = {'name': 'Документы', 'dir_name': 'documents', 'ext': documents_ext, 'files': []}
-music = {'name': 'Музыка', 'dir_name': 'audio', 'ext': music_ext, 'files': []}
-archives = {'name': 'Архиы', 'dir_name': 'archives', 'ext': archives_ext, 'files': []}
-unknown = {'name': 'Другое', 'dir_name': 'other', 'ext': [], 'files': [], 'dirs': []}
+pictures = {'name': 'Изображения', 'dir_name': 'images', 'ext': pictures_ext, 'sort': True, 'files': []}
+movies = {'name': 'Видео', 'dir_name': 'video', 'ext': movies_ext, 'sort': True, 'files': []}
+documents = {'name': 'Документы', 'dir_name': 'documents', 'ext': documents_ext, 'sort': True, 'files': []}
+music = {'name': 'Музыка', 'dir_name': 'audio', 'ext': music_ext, 'sort': True, 'files': []}
+archives = {'name': 'Архиы', 'dir_name': 'archives', 'ext': archives_ext, 'sort': True, 'files': []}
+unknown = {'name': 'Другое', 'dir_name': 'other', 'ext': [], 'sort': False, 'files': [], 'dirs': []}
+extensions_list = {'known': [], 'unknown': []}
 categories = [pictures, movies, documents, music, archives, unknown]
+
+def copy_file(root_dir, category, name):
+    pass
 
 def constant(f):
     def fset(self, value):
@@ -54,17 +58,17 @@ def normalize(in_str):
     return rx.sub('_', in_str.translate(map_cyr_to_latin))
 
 
-def get_files_recur(path, ext='*', show_all_files_dirs=typeObj.ALL, calls=1):
+def get_os_objs(path, ext='*', show_all_files_dirs=typeObj.ALL, categories_list=[]):
     if not isinstance(path, (PosixPath, WindowsPath)):
         print('ERROR:', 'path must be a PosixPath or WindowsPath')
         return False
     try:
         if show_all_files_dirs in [typeObj.ALL, typeObj.DIRS] and path.is_dir():
-            yield ('dir', path)
+            yield ('dirs', path)
         for sys_obj in path.iterdir():
             if sys_obj.is_dir():
                 # если имя каталога не в списке категорий
-                if sys_obj.name not in [cat['dir_name'] for cat in categories if cat['dir_name'] != 'other']:
+                if sys_obj.name not in categories_list:
                     # если каталог пустой нужно его удалить
                     if not list(Path(sys_obj).iterdir()):
                         try:
@@ -75,20 +79,43 @@ def get_files_recur(path, ext='*', show_all_files_dirs=typeObj.ALL, calls=1):
                         except OSError:
                             print('WARNING:', f'The {sys_obj} directory is not empty')
                     else:
-                        path = Path(Path.rename(sys_obj, Path.joinpath(sys_obj.parent, normalize(sys_obj.name))))
-                        yield from get_files_recur(path, ext=ext, show_all_files_dirs=show_all_files_dirs, calls=calls+1)
+                        sys_obj = sys_obj.rename(sys_obj.with_name(normalize(sys_obj.name)))
+                        yield from get_os_objs(sys_obj, ext=ext, show_all_files_dirs=show_all_files_dirs, categories_list=categories_list)
             else:
-                name, extension = sys_obj.stem, sys_obj.suffix
-                path = Path(Path.rename(sys_obj, Path.joinpath(sys_obj.parent, normalize(name)+extension)))
+                sys_obj = sys_obj.rename(str(sys_obj.with_name(normalize(sys_obj.stem)))+sys_obj.suffix)
                 if sys_obj.match('**'+ext) and show_all_files_dirs in [typeObj.ALL, typeObj.FILES]:
-                    yield ('file', sys_obj)
+                    yield ('files', sys_obj)
     except FileNotFoundError as e:
         if e.errno == 2:
             print('ERROR:', 'No such file or directory')
 
 
 _dir = Path(r"/home/user1/1. COURSES/goit_github/1.test_")
-(Path(Path.joinpath(_dir, cat['dir_name'])).mkdir(parents=True, exist_ok=True) for cat in categories if cat['dir_name'] != 'other')
-gen = get_files_recur(_dir)
-for g in gen:
-    g
+# список категорий, которые подлежат сортировке
+cat_dirs = [cat['dir_name'] for cat in categories if cat['sort'] == True]
+(Path(Path.joinpath(_dir, cat)).mkdir(parents=True, exist_ok=True) for cat in cat_dirs)
+# генерируем список файлов, содержащихся в целевом каталоге
+gen = get_os_objs(_dir, show_all_files_dirs=typeObj.FILES, categories_list=cat_dirs)
+
+for type_obj, path_name in gen:
+    if type_obj == typeObj.FILES:
+        name_ext, ext = path_name.name, path_name.suffix[1:]
+        for cat in [c for c in categories if c['sort'] == True]:
+            know_ext = False
+            if ext in cat['ext']:
+                cat['files'].append(name_ext)
+                know_ext = True
+                break
+        if know_ext:
+            if ext not in extensions_list['known']:
+                extensions_list['known'].append(ext)
+        else:
+            if ext not in extensions_list['unknown']:
+                extensions_list['unknown'].append(ext)
+
+print(f"В каталоге \n{_dir}\nимеются следующие файлы:")
+for cat in [c for c in categories if c['sort'] == True]:
+    print(f"{cat['name']:<12}: {', '.join(cat['files'])}")
+print(f"{'Каталоги':<12}: {', '.join(unknown['dirs'])}")
+print('Список известных расширений:', ', '.join(extensions_list['known']))
+print('Список неизвестных расширений:', ', '.join(extensions_list['unknown']))
