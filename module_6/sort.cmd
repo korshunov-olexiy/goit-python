079b50a52bce251966fdd1ce624ecbcd58533301
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-10-08 09:41:31 +0300

    main

--- module_6/sort.cmd
+++
@@ -1,2 +0,0 @@
-python sorting_files.py "d:/1.Š“‘›_goIT/„‡/Tech Skills/goit-python/1.TEST_/"
-pause
--- module_6/sort.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 sorting_files.py "/home/user1/1. COURSES/goit_github/1.test_"
-
