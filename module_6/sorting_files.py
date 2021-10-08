81a7568378dd5699eafd28dd9179a850520617f9
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-10-08 10:49:22 +0300

    main

--- module_6/sorting_files.py
+++ module_6/sorting_files.py
@@ -4,37 +4,13 @@ from sys import argv
 from shutil import unpack_archive
 import re
 
-try:
-    # ACHTUNG: If WinError 122 is thrown, then the path contains the wrong slashes. Necessary "/"
-    _dir = Path(argv[1])
-except IndexError:
-    print("Fatal error: Use the target directory path parameter to run the program.")
-    exit()
-except Exception as err:
-    print(f"Fatal error: {str(err)}")
-    exit()
-
-pictures_ext = ['bmp', 'jpeg', 'png', 'jpg', 'gif']
-movies_ext = ['avi', 'mp4', 'mov']
-documents_ext = ['pdf', 'doc', 'docx', 'txt']
-music_ext = ['mp3', 'ogg', 'wav', 'amr']
-archives_ext = ['zip', 'gz', 'tar']
-
-# Dictionaries for each category. The 'tag' key is used in file handling functions. (move_media, move_archives)
-pictures = {'name': 'Изображения', 'dir_name': 'images', 'ext': pictures_ext, 'sort': True, 'tag': 'media', 'files': []}
-movies = {'name': 'Видео', 'dir_name': 'video', 'ext': movies_ext, 'sort': True, 'tag': 'media', 'files': []}
-documents = {'name': 'Документы', 'dir_name': 'documents', 'ext': documents_ext, 'sort': True, 'tag': 'media', 'files': []}
-music = {'name': 'Музыка', 'dir_name': 'audio', 'ext': music_ext, 'sort': True, 'tag': 'media', 'files': []}
-archives = {'name': 'Архиы', 'dir_name': 'archives', 'ext': archives_ext, 'sort': True, 'tag': 'archive', 'files': []}
-unknown = {'name': 'Другое', 'dir_name': 'other', 'ext': [], 'sort': False, 'tag': 'other', 'files': [], 'dirs': []}
-extensions_list = {'known': [], 'unknown': []}
-categories = [pictures, movies, documents, music, archives, unknown]
 
 # Decorator for file type constants.
 def constant(f):
     def fset(self, value): raise TypeError
     def fget(self): return f()
     return property(fget, fset)
+
 
 # Classes for choosing constants 'all', 'files', 'dirs'
 class TypeOfShowObject(object):
@@ -47,17 +23,8 @@ class TypeOfShowObject(object):
     @constant
     def ERROR(): return 'error'
 
+
 typeObj = TypeOfShowObject()
-
-# Character transcoding table.
-table_symbols = ('абвгґдеєжзиіїйклмнопрстуфхцчшщюяыэАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯЫЭьъЬЪ',
-                 (*(u'abvhgde'), 'ye', 'zh', *(u'zyi'), 'yi', *(u'yklmnoprstuf'), 'kh', 'ts',
-                 'ch', 'sh', 'shch', 'yu', 'ya', 'y', 'ye', *(u'ABVHGDE'), 'Ye', 'Zh', *(u'ZYI'),
-                 'Yi', *(u'YKLMNOPRSTUF'), 'KH', 'TS', 'CH', 'SH', 'SHCH', 'YU', 'YA', 'Y', 'YE',
-                 *(u'_'*4)))
-
-# Converting the lookup table to a dictionary for the str.translate property.
-map_cyr_to_latin = {ord(src): dest for src, dest in zip(*table_symbols)}
 
 def del_empty_dirs(path, sort_dirs):
     """Recursively deleting empty directories in the target directory path.
@@ -146,63 +113,106 @@ def sort_dir(path, ext='*', show_all_files_dirs=typeObj.ALL, categories_list=[])
             print(typeObj.ERROR, 'No such file or directory')
 
 
-# Dictionary: directory_name and a list of category extensions.
-cat_dirs = {cat['dir_name']: cat['ext'] for cat in categories}
+if __name__ == "__main__":
+    try:
+        # Attention: If WinError 122 is thrown, then the path contains the wrong slashes. Necessary "/"
+        _dir = Path(argv[1])
+        if not (_dir.exists() and _dir.is_dir()):
+            print("The parameter passed is not a directory, or the directory does not exist.")
+            exit()
+    except IndexError:
+        print("Fatal error: Use the target directory path parameter to run the program.")
+        exit()
+    except Exception as err:
+        print(f"Fatal error: {str(err)}")
+        exit()
 
-# By extension, we determine the name of the directory in the categories.
-dir_of_category = lambda extension: ''.join([k for k,v in cat_dirs.items() if extension in v])
+    pictures_ext = ['bmp', 'jpeg', 'png', 'jpg', 'gif']
+    movies_ext = ['avi', 'mp4', 'mov']
+    documents_ext = ['pdf', 'doc', 'docx', 'txt']
+    music_ext = ['mp3', 'ogg', 'wav', 'amr']
+    archives_ext = ['zip', 'gz', 'tar']
+    # Dictionaries for each category. The 'tag' key is used in file handling functions. (move_media, move_archives)
+    pictures = {'name': 'Изображения', 'dir_name': 'images', 'ext': pictures_ext, 'sort': True, 'tag': 'media', 'files': []}
+    movies = {'name': 'Видео', 'dir_name': 'video', 'ext': movies_ext, 'sort': True, 'tag': 'media', 'files': []}
+    documents = {'name': 'Документы', 'dir_name': 'documents', 'ext': documents_ext, 'sort': True, 'tag': 'media', 'files': []}
+    music = {'name': 'Музыка', 'dir_name': 'audio', 'ext': music_ext, 'sort': True, 'tag': 'media', 'files': []}
+    archives = {'name': 'Архиы', 'dir_name': 'archives', 'ext': archives_ext, 'sort': True, 'tag': 'archive', 'files': []}
+    unknown = {'name': 'Другое', 'dir_name': 'other', 'ext': [], 'sort': False, 'tag': 'other', 'files': [], 'dirs': []}
+    extensions_list = {'known': [], 'unknown': []}
+    categories = [pictures, movies, documents, music, archives, unknown]
+    # Character transcoding table.
+    table_symbols = ('абвгґдеєжзиіїйклмнопрстуфхцчшщюяыэАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЮЯЫЭьъЬЪ',
+                    (*(u'abvhgde'), 'ye', 'zh', *(u'zyi'), 'yi', *(u'yklmnoprstuf'), 'kh', 'ts',
+                    'ch', 'sh', 'shch', 'yu', 'ya', 'y', 'ye', *(u'ABVHGDE'), 'Ye', 'Zh', *(u'ZYI'),
+                    'Yi', *(u'YKLMNOPRSTUF'), 'KH', 'TS', 'CH', 'SH', 'SHCH', 'YU', 'YA', 'Y', 'YE',
+                    *(u'_'*4)))
+    # Converting the lookup table to a dictionary for the str.translate property.
+    map_cyr_to_latin = {ord(src): dest for src, dest in zip(*table_symbols)}
+    # Dictionary: directory_name and a list of category extensions.
+    cat_dirs = {cat['dir_name']: cat['ext'] for cat in categories}
+    # By extension, we determine the name of the directory in the categories.
+    dir_of_category = lambda extension: ''.join([k for k,v in cat_dirs.items() if extension in v])
+    # List of extensions by tag 'media' from the category dictionary.
+    ext_media_list = []
+    # List of extensions by tag 'archive' from the category dictionary.
+    ext_archive_list = []
+    for cat in categories:
+        tag = cat['tag']
+        for c in cat['ext']:
+            if tag == 'media':
+                ext_media_list.append(c)
+            elif tag == 'archive':
+                ext_archive_list.append(c)
 
-# List of extensions by tag 'media' from the category dictionary.
-ext_media_list = [c for cat in categories for c in cat['ext'] if cat['tag'] == 'media']
-# List of extensions by tag 'archive' from the category dictionary.
-ext_archive_list = [c for cat in categories for c in cat['ext'] if cat['tag'] == 'archive']
-
-for dir_name in cat_dirs.keys():
     # Create subdirectories for the categories in the target directory.
-    _dir.joinpath(dir_name).mkdir(parents=True, exist_ok=True)
+    for dir_name in cat_dirs.keys():
+        _dir.joinpath(dir_name).mkdir(parents=True, exist_ok=True)
 
-# We generate a list of files contained in the target directory.
-gen = sort_dir(_dir, show_all_files_dirs=typeObj.FILES, categories_list=cat_dirs.keys())
+    # We generate a list of files contained in the target directory.
+    src_files_gen = sort_dir(_dir, show_all_files_dirs=typeObj.FILES, categories_list=cat_dirs.keys())
+    # We sort the files: with the media and archive tags according to our own rules.
+    for type_obj, path_name in src_files_gen:
+        # If the object type is "error", print a message to the console and end the loop. 
+        if type_obj == typeObj.ERROR:
+            print(typeObj.ERROR, path_name)
+            break
+        elif type_obj == typeObj.FILES:
+            name_ext, ext = path_name.name, path_name.suffix[1:]
+            # By the file extension path_name,
+            # we determine the name of the directory into which we will move the files.
+            cat_dir = dir_of_category(ext)
+            # Add the found filenames to the list of files for a specific category.
+            for cat in categories:
+                if cat['dir_name'] == cat_dir:
+                    cat['files'].append(name_ext)
+            # If the file extension is in the list of extensions
+            # (which means the calculated directory name for extensions! = ''). 
+            if cat_dir:
+                # If it is a file from the 'media' tag (document, music, video, image).
+                if ext in ext_media_list:
+                    move_media(_dir.joinpath(cat_dir), path_name)
+                # If it is an archive (tag 'archive').
+                elif ext in ext_archive_list:
+                    move_archives(_dir.joinpath(cat_dir), path_name)
+                # Add the file extension to the list of known extensions.
+                if ext not in extensions_list['known']:
+                    extensions_list['known'].append(ext)
+            # If you find a file with an unknown extension.
+            else:
+                # Add the file extension to the list of unknown extensions.
+                if ext not in extensions_list['unknown']:
+                    extensions_list['unknown'].append(ext)
 
-# We sort the files: with the media and archive tags according to our own rules.
-for type_obj, path_name in gen:
-    # If the object type is "error", print a message to the console and end the loop. 
-    if type_obj == typeObj.ERROR:
-        print(typeObj.ERROR, path_name)
-        break
-    elif type_obj == typeObj.FILES:
-        name_ext, ext = path_name.name, path_name.suffix[1:]
-        # By the file extension path_name,
-        # we determine the name of the directory into which we will move the files.
-        cat_dir = dir_of_category(ext)
-        # Add the found filenames to the list of files for a specific category.
-        [cat['files'].append(name_ext) for cat in categories if cat['dir_name'] == cat_dir]
-        # If the file extension is in the list of extensions
-        # (which means the calculated directory name for extensions! = ''). 
-        if cat_dir:
-            # If it is a file from the 'media' tag (document, music, video, image).
-            if ext in ext_media_list:
-                move_media(_dir.joinpath(cat_dir), path_name)
-            # If it is an archive (tag 'archive').
-            elif ext in ext_archive_list:
-                move_archives(_dir.joinpath(cat_dir), path_name)
-            # Add the file extension to the list of known extensions.
-            if ext not in extensions_list['known']:
-                extensions_list['known'].append(ext)
-        # If you find a file with an unknown extension.
-        else:
-            # Add the file extension to the list of unknown extensions.
-            if ext not in extensions_list['unknown']:
-                extensions_list['unknown'].append(ext)
+    # We delete empty directories in the target directory
+    # (except directories for categories).
+    del_empty_dirs(_dir, cat_dirs.keys())
 
-# We delete empty directories in the target directory
-# (except directories for categories).
-del_empty_dirs(_dir, cat_dirs.keys())
+    # We display the result of the program in a formatted form.
+    print(f"В каталоге \n{_dir}\nимеются следующие файлы:")
 
-# We display the result of the program in a formatted form.
-print(f"В каталоге \n{_dir}\nимеются следующие файлы:")
-for cat in [c for c in categories if c['sort'] == True]:
-    print(f"{cat['name']:<12}: {', '.join(cat['files'])}")
-# print(f"{'Каталоги':<12}: {', '.join(unknown['dirs'])}")
-print('Список известных расширений:', ', '.join(extensions_list['known']))
-print('Список неизвестных расширений:', ', '.join(extensions_list['unknown']))
+    for cat in [c for c in categories if c['sort'] == True]:
+        print(f"{cat['name']:<12}: {', '.join(cat['files'])}")
+
+    print('Список известных расширений:', ', '.join(extensions_list['known']))
+    print('Список неизвестных расширений:', ', '.join(extensions_list['unknown']))
