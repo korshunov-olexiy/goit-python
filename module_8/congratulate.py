28984f1a7e5222254a6b0af19ff75dcbfa5bc309
korshunov.olexiy <korshunov.olexiy@gmail.com>
2021-10-18 14:44:28 +0300

    main

--- module_8/congratulate.py
+++ module_8/congratulate.py
@@ -1,15 +1,24 @@
 from datetime import datetime, timedelta, date
 
 
-wks = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+WKS = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+
+# print(users[0])
+# .strftime('%d.%m.%Y')
+# print(date.today().weekday())
+
+def congratulate(file_name):
+    """Displays a list of birthday people who need to be congratulated this week.
+
+    Args:
+        file_name ([str]): The name of the file where names and birthdays are stored.
+    """
+    users = []
+    with open(file_name, 'r', encoding='utf-8') as file_users:
+        for line in file_users:
+            name, birth = line.strip().split("\t")
+            users.append({'name': name, 'birthday': datetime.strptime(birth, "%d.%m.%Y").date()})
+
 wk_brth = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
 
-users = []
-with open('users.txt', 'r', encoding='utf-8') as file_users:
-    for line in file_users:  #.read().splitlines():
-        name, birth = line.strip().split("\t")
-        users.append({'name': name, 'birthday': datetime.strptime(birth, "%d.%m.%Y").date()})
-
-print(users[0])
-# .strftime('%d.%m.%Y')
-print(date.today().weekday())
+# congratulate('users.txt')
