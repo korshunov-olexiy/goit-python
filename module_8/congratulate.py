30d24935262c55d684e191a4b6445786a64e77fd
korshunov.olexiy <korshunov.olexiy@gmail.com>
2021-10-18 15:06:52 +0300

    main

--- module_8/congratulate.py
+++ module_8/congratulate.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, date
 
 
-WKS = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+WEEKDAYS = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
 
 # print(users[0])
 # .strftime('%d.%m.%Y')
@@ -13,6 +13,11 @@ def congratulate(file_name):
     Args:
         file_name ([str]): The name of the file where names and birthdays are stored.
     """
+    # current date without year.
+    cur_date = datetime.today().strftime('%d.%m.')
+    # List of days of the week from current date to Sunday (weekday = 6)
+    week_dates = range(datetime.today().weekday, 7)
+    # list with users dict
     users = []
     with open(file_name, 'r', encoding='utf-8') as file_users:
         for line in file_users:
