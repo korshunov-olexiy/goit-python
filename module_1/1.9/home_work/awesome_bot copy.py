38695f5295e6e1c5cd57c7c687181f377c149f2d
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-10-22 21:24:38 +0300

    main

--- module_1/1.9/home_work/awesome_bot copy.py
+++ module_1/1.9/home_work/awesome_bot copy.py
@@ -2,12 +2,6 @@
 
 def input_error(func):
     def check_error(input_str):
-        if func.__name__ == "get_input_command":
-            result = func(input_error)
-            return result
-        elif func.__name__ in ['cmd_hello', 'cmd_show_all']:
-            result = func()
-            return result
         try:
             result = func
             return result
@@ -19,33 +13,37 @@ def input_error(func):
             print(err_idx)
 
 @input_error
-def cmd_hello():
+def cmd_hello(input_str):
     msg = "How can I help you?"
+    print(msg)
     return msg
 
 @input_error
 def cmd_add(input_str):
-    name, phone = input_str.split()
-    address_book[name.strip().capitalize()] = phone.strip()
+    name_cmd = commands_list[1]
+    name, phone = input_str[len(name_cmd):].split()
+    address_book.append({name.strip(), phone.strip()})
     return f"User {name} with phone number {phone} has been added to address book"
 
 @input_error
 def cmd_change(input_str):
-    name, phone = input_str.split()
-    address_book[name.strip().capitalize()] = phone.strip()
+    name_cmd = commands_list[2]
+    name, phone = input_str[len(name_cmd):].split()
+    address_book[name.strip()] = phone.strip()
     return f"The phone number for {name} has been changed"
 
 @input_error
 def cmd_phone(input_str):
-    name = input_str.split().capitalize()
-    finding_phone = address_book.get(name)
+    name_cmd = commands_list[3]
+    name, phone = input_str[len(name_cmd):].split()
+    finding_phone = address_book.get(name.strip())
     if finding_phone:
         return f"The {name} has a phone number as {finding_phone}"
     else:
         return "This user was not found in the database"
 
 @input_error
-def cmd_show_all():
+def cmd_show_all(input_str):
     res = []
     for user_info in address_book:
         name, phone = user_info.items()
@@ -60,17 +58,12 @@ commands_func = {cmd: func for cmd, func in zip(commands_list, functions_list)}
 def get_handler(operator):
     return commands_func[operator]
 
-def get_input_command(input_str):
-    cmd = ''.join([c for c in commands_list if input_msg.startswith(c)])
-    if cmd:
-        print(get_handler(cmd)(input_msg))
-    else:
-        print("Sorry, I could not recognize the entered command!")
-
 if __name__ == "__main__":
     input_msg = input("Hello, please enter one of the commands: hello, add, change, phone or show all\n: ").lower().strip()
     while input_msg != ['good bye', 'close', 'exit']:
-        get_input_command(input_msg)
+        cmd = ''.join([c for c in commands_list if input_msg.startswith(c)])
+        if cmd:
+            print(get_handler(cmd)(input_msg))
         input_msg = input("Hello, please enter one of the commands: hello, add, change, phone or show all\n: ").lower().strip()
     
     print("Have a nice day... Good bye!")
