447c5958a32d84c331f4ba95b31b1d4dbf2699f6
Olexiy Korshunov <korshunov.olexiy@gmail.com>
2021-10-23 12:27:48 +0300

    Create sanitize_phone_number.py

---
+++ module_1/1.5/sanitize_phone_number.py
@@ -0,0 +1,5 @@
+def sanitize_phone_number(phone):
+    repl_chr = ['(', '-', ')', '+', ' ']
+    return ''.join(filter(lambda s: s not in repl_chr, phone))
+
+print( sanitize_phone_number("    +38 (095) 3245678   ") )
