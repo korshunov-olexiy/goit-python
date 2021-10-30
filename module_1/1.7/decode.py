2b6e89e59485cc2865c238243e847ec9a14c4a41
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-10-30 11:55:14 +0300

    main

--- module_1/1.7/decode.py
+++ module_1/1.7/decode.py
@@ -8,12 +8,8 @@
 def decode(data):
     if data == []:
         return data
-    if isinstance(data[1], int):
-        return list(data[0]*data[1]) + decode(data[2:])
-    else:
-        return decode(data[1:])
+    return list(data[0]*data[1]) + decode(data[2:])
 
 
 lst = ['X', 3, 'Z', 2, 'X', 2, 'Y', 3, 'Z', 2]
 print(decode(lst))
-# result: ["X", "X","X", "Z", "Z", "X", "X", "Y", "Y", "Y", "Z", "Z"]
