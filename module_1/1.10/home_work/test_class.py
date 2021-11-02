1dc133f5cea52cc9bc7f74f25e223ce7f5116e7c
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-11-02 17:20:10 +0200

    main

--- module_1/1.10/home_work/test_class.py
+++ module_1/1.10/home_work/test_class.py
@@ -1,18 +1,24 @@
 from collections import UserDict, UserList
 
 
-print([d for d in dir(UserDict) if not d.startswith("_")])
+class Name:
+    def __init__(self, name):
+        self.value = name
+
+class Phone:
+    def __init__(self, phones):
+        if isinstance(phones, str):
+            self.values = [p.replace(' ', '') for p in phones.split(',')]
+        else:
+            self.values = [p.replace(' ', '') for p in phones]
 
 class Record(UserDict):
     def __init__(self, name, phones = [], address = None):
-        self.data = {'name': name}
-        phones = [phones] if isinstance(phones, str) else phones
-        self.data.update({'phones': phones})
-        self.data.update({'address': address})
+        self.data = {'name': Name(name), 'phones': Phone(phones)}
 
 
-john = Record('John', '23-23-121')
-bob = Record('Bob', ['123-345465-2351', '24-456-234-976'])
+john = Record('John', '23-23-121, 2-234-2-34-2')
+bob = Record('Bob', ['123-345-2351', '24-456-234-976'])
 
-print(john)
-print(bob)
+print(john['name'].value)
+print(bob['phones'].values)
