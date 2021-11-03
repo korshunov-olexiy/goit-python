ab44bdc88081918d173a7de03e205f88e76e55f2
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-11-03 16:38:33 +0200

    main

--- module_1/1.10/home_work/bot_classes.py
+++ module_1/1.10/home_work/bot_classes.py
@@ -1,55 +1,54 @@
-from collections import UserDict, UserList
+from collections import UserDict
 
-'''
-обязательное поле с именем
-'''
-class Name:
-    pass
 
-'''
-родительский класс для всех полей
-'''
 class Field:
-    pass
+    '''
+    Field class is parent for all fields in Record class
+    '''
 
+class Name(Field):
+    '''
+    Name class for storage name's field
+    '''
 
-'''
-Не обязательное поле с телефоном
-и таких одна запись (Record) может содержать несколько полей
-'''
-class Phones(UserList):
-    def add(self, value):
-        self.data.append(value)
+class Phone(Field):
+    '''
+    Phone class for storage phone's field
+    '''
 
-'''
-добавление, удаление, редактирование
-необязательных полей
-и хранение обязательного поля name
-'''
-class Record:
-    def __init__(self, name, phone = []):
-        self.name = name
-        phones = self.data.get('phones', Phones())
-        phones.add(phone)
-        #self.phones.append(Phones.add(phone))
+class Record(UserDict):
+    '''
+    Record class responsible for the logic of adding/removing/editing fields
+    '''
+    def __init__(self, name, phones = []):
+        self.data = {'name': name, 'phone': phones}
+    
+    def add_field(self, field_name, value):
+        self.data.update({field_name: value})
+    
+    def del_field(self, field_name):
+        self.data.pop(field_name)
+    
+    def edit_field(self, field_name, value):
+        self.data[field_name] = value
 
 
 class AddressBook(UserDict):
-    # добавляет объект Record в self.data
-    def add_record(self, record=Record):
-        phones = self.data.get([record.phones], [])
-        phones.append(record)
-        self.data[record.phones] = phones
-    
-    def find_record(self, value):
-        keys = []
+    def add_record(self, user_name, phones = []):
+        record = Record(user_name, phones)
+        self.data.update({user_name: record})
+        return record
 
 
-class Point:
-    x, y = 12, 22
-    def setCoord(self, x, y):
-        self.a, self.b = x,y
 
-p = Point()
-p.setCoord(10,20)
-print(p.x, p.y, p.__dict__)
+# -=- Usage example -=-
+# create address book
+ad = AddressBook()
+# add user in address book
+john = ad.add_record('john')
+# add another user in address book
+bob = ad.add_record('bob')
+# add new field for user john in address book
+john.add_field('address', 'Kurska, 56')
+# edit field for user john in address book
+john.edit_field('address', 'Illinska, 156')
