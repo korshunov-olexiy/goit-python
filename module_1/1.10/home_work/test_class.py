d1e310d31d030cdd6d0a7346b0cb58c601efe0a8
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-11-03 16:37:48 +0200

    main

--- module_1/1.10/home_work/test_class.py
+++ module_1/1.10/home_work/test_class.py
@@ -1,27 +1,54 @@
-from collections import UserDict, UserList
+from collections import UserDict
 
 
 class Field:
-    pass
+    '''
+    Field class is parent for all fields in Record class
+    '''
 
 class Name(Field):
-    def __init__(self, name):
-        self.value = name
+    '''
+    Name class for storage name's field
+    '''
 
 class Phone(Field):
-    def __init__(self, phones):
-        if isinstance(phones, str):
-            self.values = [p.replace(' ', '') for p in phones.split(',')]
-        else:
-            self.values = [p.replace(' ', '') for p in phones]
+    '''
+    Phone class for storage phone's field
+    '''
 
 class Record(UserDict):
+    '''
+    Record class responsible for the logic of adding/removing/editing fields
+    '''
     def __init__(self, name, phones = []):
-        self.data = {'name': Name(name), 'phones': Phone(phones)}
+        self.data = {'name': name, 'phone': phones}
+    
+    def add_field(self, field_name, value):
+        self.data.update({field_name: value})
+    
+    def del_field(self, field_name):
+        self.data.pop(field_name)
+    
+    def edit_field(self, field_name, value):
+        self.data[field_name] = value
 
 
-john = Record('John', '23-23-121, 2-234-2-34-2')
-bob = Record('Bob', ['123-345-2351', '24-456-234-976'])
+class AddressBook(UserDict):
+    def add_record(self, user_name, phones = []):
+        record = Record(user_name, phones)
+        self.data.update({user_name: record})
+        return record
 
-print(john['name'].value)
-print(bob['phones'].values)
+
+
+# -=- Usage example -=-
+# create address book
+ad = AddressBook()
+# add user in address book
+john = ad.add_record('john')
+# add another user in address book
+bob = ad.add_record('bob')
+# add new field for user john in address book
+john.add_field('address', 'Kurska, 56')
+# edit field for user john in address book
+john.edit_field('address', 'Illinska, 156')
