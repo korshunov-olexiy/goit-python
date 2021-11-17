7bd74cf694ac8e0df0a7f52306b8c79f22adab9d
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-11-17 10:42:37 +0200

    main

--- module_1/1.10/home_work/adressbook_classes.py
+++ module_1/1.10/home_work/adressbook_classes.py
@@ -5,9 +5,8 @@ class Record:
     """Record class responsible for the logic of adding/removing/editing fields
     Only one name but many phone numbers"""
 
-    def __init__(self, name, phones=['']):
+    def __init__(self, name:str, phones=['']):
         self.name = Name(name)
-        #self.phone = list(map(lambda p: Phone(name, p), phone))
         self.phones = Phone(name, phones)
 
     def add_phone(self, phone):
@@ -23,60 +22,56 @@ class Record:
 
 class Field:
     '''Field class is parent for all fields in Record class'''
-    def __init__(self, name, phones=['']):
+    def __init__(self, name:str, phones=['']):
         self.value = name.capitalize()
         self.phones = phones
 
 
 class Name(Field):
     '''Name class for storage name's field'''
-    def __init__(self, name):
-        # reload parent dunder method __init__
-        super().__init__(name)
 
 
 class Phone(Field):
     '''Phone class for storage phone's field'''
-    def __init__(self, name, phone):
-        super().__init__(name, phone)
 
 
 class AddressBook(UserDict):
-    """All contacts data"""
-    # передаем в add_record !!объект Record
+    '''Add new instance of Record class in AddressBook'''
     def add_record(self, record:Record):
         self.data[record.name.value] = record
 
-    def change_record(self, record):
-        for this_name, this_record in self.data.items():
-            if this_name == record[0]:
-                contact_phones = list(map(lambda phone_number: phone_number.phone, this_record.phone))
-                if record[1] not in contact_phones:
-                    ''''''
-                    #raise UnknownPhoneError
-                self.data[this_name].change_phone(contact_phones.index(record[1]), record[2])
-                return self.data[this_name]
-        # raise UnknownContactError
+    '''Change record name & attribute name in Name class'''
+    def change_record_name(self, record_name:str, new_name:str):
+        record_name = record_name.capitalize()
+        new_name = new_name.capitalize()
+        if self.data.get(record_name):
+            # change name of record
+            self.data[new_name] = self.data.pop(record_name)
+            # change name in Name class
+            self.data[new_name].name.value = new_name
+            return True
+        return False
 
-    def find_record(self, required_name):
-        for this_name, this_record in self.data.items():
-            if this_name == required_name:
-                return this_record
-        return None
+    '''Find record by name'''
+    def find_record(self, required_name:str):
+        return self.data.get(required_name.capitalize(), None)
 
 
 
 if __name__ == "__main__":
     # -=- Usage example -=-
-    ad = AddressBook()
+    ab = AddressBook()
     john = Record('john', ['144-255', '202-324'])
     #print('john:', john.phones.phones)
-    ad.add_record(john)
+    ab.add_record(john)
     bob = Record('bob', ['325-146'])
-    ad.add_record(bob)
-    for name, rec in ad.data.items():
+    ab.add_record(bob)
+    for name, rec in ab.data.items():
         print(name, rec.name.value, rec.phones.phones)
-        #print(name, rec.name.value, ', '.join([r.value for r in rec.phone]))
+    
+    ab.change_record_name('bob', 'smith')
+    print(ab.__dict__)
+    print(ab.find_record('smith').name.value)
     # add new field for user john in address book
     #john.add_field('address', 'Kurska, 56')
     # edit field for user john in address book
