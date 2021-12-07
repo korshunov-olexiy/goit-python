f319bc5c290695d48c2f7fa03f6f93ac83c890c8
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-12-07 16:30:59 +0200

    main

--- module_1/final_homework/testing_manager.py
+++ module_1/final_homework/testing_manager.py
@@ -1,8 +1,12 @@
 action_cmd = ['help', 'hello', 'add', 'change', 'phone', 'show all']
-functions_list = ['cmd_help', 'cmd_hello', 'cmd_add', 'cmd_change', 'cmd_phone', 'cmd_show_all']
 exit_cmd = ['good bye', 'close', 'exit']
+list_of_function = ['cmd_help', 'cmd_hello', 'cmd_add', 'cmd_change', 'cmd_phone', 'cmd_show_all'] + ['cmd_exit']*len(exit_cmd)
+
 user_cmd = {'close', 'add'}
-commands_func = {cmd: func for cmd, func in zip(action_cmd, functions_list)}
+commands_func = {cmd: func for cmd, func in zip(action_cmd+exit_cmd, list_of_function)}
+
+print(commands_func)
+
 
 def analize_input_msg(cmd_user, cmd_action, cmd_exit):
     action_cmd = set(cmd_action)
@@ -14,14 +18,15 @@ def analize_input_msg(cmd_user, cmd_action, cmd_exit):
     if in_exit and in_action:
         '''check action or exit'''
         print("Choice command:")
+        commands_func.update({cmd_exit[0]: None})
         for one_cmd_user in cmd_user:
-            print(f"{one_cmd_user}")
+            '''run command choces user from menu'''
     elif in_exit and not in_action:
         '''exit program'''
     elif not in_exit and in_action:
         '''check what is action'''
     elif not in_exit and not in_action:
-        '''print -> not know command'''
+        '''print(not know command)'''
 
 
 #  1 & 1  =  1
