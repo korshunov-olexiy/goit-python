3f40f203c87b08437e9b8ac2508e0fa8b7315b9e
korshunov-olexiy <korshunov.olexiy@gmail.com>
2021-12-29 11:50:30 +0200

    update build.cmd

--- module_1/final_homework/build.cmd
+++ module_1/final_homework/build.cmd
@@ -1,6 +1,27 @@
+@echo off
 REM MANUAL: https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+IF "%VIRTUAL_ENV%" == "" (
+  echo "Virtual environment not activated. Try activate..."
+  .\env\Scripts\activate.bat
+)
+
+IF %ERRORLEVEL% NEQ 0 (
+  echo "Virtual environment not found in 'env' directory. Try configure..."
+  python -m venv env
+  echo "Upgrade pip..."
+  python.exe -m pip install --upgrade pip
+)
+
+REM CHECK IF MODULE BUILD INSTALLED
+python -c "import build"
+IF %ERRORLEVEL% NEQ 0 (
+  echo "Module 'build' not found. Try install..."
+  python -m pip install build
+)
+
 mkdir dist
-REM uninstall package
+REM try uninstall old version of package
 pip uninstall personal-manager -y
 python -m build
 REM install package
@@ -10,3 +31,5 @@ rmdir /S /Q .\dist
 rmdir /S /Q .\src\personal_manager.egg-info
 REM clear pip cache
 pip cache purge
+
+@echo on
