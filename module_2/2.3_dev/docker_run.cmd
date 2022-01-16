46c852d80c046492c47125c8e9af4292a135e674
Olexii Korshunov <korshunov.olexiy@gmail.com>
2022-01-16 06:37:40 +0200

    update docker_run.cmd

--- module_2/2.3_dev/docker_run.cmd
+++ module_2/2.3_dev/docker_run.cmd
@@ -1,1 +1,11 @@
-docker run personal-manager
+@REM REMOVE ALL IMAGES: docker image prune -a
+
+@REM RUN ONCE: Create container and run it
+@REM docker run -it personal-manager
+
+@REM START CONTAINER
+docker start personal-manager
+@REM EXEC COMMAND "personal-manager" IN CONTAINER AND BOUND TERMINAL
+docker container exec -it personal-manager personal-manager
+@REM STOP CONTAINER
+docker stop personal-manager
