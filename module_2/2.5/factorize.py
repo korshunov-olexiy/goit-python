3abc0968567f9ac0a2606ecbcd2220c6555287d7
Olexii Korshunov <korshunov.olexiy@gmail.com>
2022-01-20 22:04:52 +0200

    create factorize_sync

--- module_2/2.5/factorize.py
+++ module_2/2.5/factorize.py
@@ -1,18 +1,41 @@
 from multiprocessing import Process
 from timebudget import timebudget
+from threading import RLock, Thread
+from typing import Type
+
+class SyncThread(Thread):
+
+    def __init__(self, wrapper: Type[object], num: int) -> None:
+        self.wrapper = wrapper
+        self.num = num
+        super().__init__()
+
+    def run(self):
+        self.wrapper(self.num)
 
 
 @timebudget
 def factorize(*number):
     result = []
     for num in number:
-        result.append([i for i in range(1, num+1)  if not num % i])
+        result.append([n for n in range(1, num+1)  if not num % n])
     return result
 
 
-a, b, c, d  = factorize(128, 255, 99999, 10651060)
+@timebudget
+def factorize_sync(*number):
+    result = []
 
-assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    def wr(num):
+        result.append([n for n in range(1, num+1) if not num % n])
+
+    for num in number:
+        s = SyncThread(wr, num)
+        s.start()
+    while s.is_alive():
+        """"""
+    return result
+
+
+# factorize(128, 255, 99999, 10651060)
+print(factorize_sync(128, 255, 99999, 10651060))
