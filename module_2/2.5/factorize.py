baac9995391c7dcef25437df5bb5cd3848534f63
Olexii Korshunov <korshunov.olexiy@gmail.com>
2022-01-23 17:12:01 +0200

    update factorize

--- module_2/2.5/factorize.py
+++ module_2/2.5/factorize.py
@@ -1,8 +1,6 @@
-from multiprocessing import Process
-from threading import RLock, Thread
-from time import sleep
-from typing import Type
-from sys import exit
+from multiprocessing import Process, Manager, managers, Pool, cpu_count
+from threading import Thread
+from typing import List, Type
 from timebudget import timebudget
 
 
@@ -17,48 +15,53 @@ class SyncThread(Thread):
         self.wrapper(self.num)
 
 
-@timebudget
-def factorize(*number):
-    result = []
-    for num in number:
-        result.append([n for n in range(1, num+1)  if not num % n])
-    return result
+def factorize(numbers: List[int], array: Type[managers.ListProxy] = None):
+    global RESULT
+    if array is None:
+        for number in numbers:
+            RESULT.append([n for n in range(1, number+1)  if not number % n])
+        return RESULT
+    else:
+        array.append([n for n in range(1, numbers+1)  if not numbers % n])
 
 
 @timebudget
-def factorize_sync(*number):
-    result = []
-
-    def wr(num):
-        result.append([n for n in range(1, num+1) if not num % n])
+def factorize_sync(numbers: List[int]):
     dict_sync = {}
-    for num in number:
-        dict_sync[num] = SyncThread(wr, num)
-        dict_sync[num].start()
+    global RESULT
+    RESULT = []
+    for number in numbers:
+        dict_sync[number] = SyncThread(factorize, (number,))
+        dict_sync[number].start()
     while not all([not res.is_alive() for res in dict_sync.values()]):
         """"""
-    return result
+    return RESULT
 
-def f(number):
-    print([n for n in range(1, number+1)  if not number % n])
-    exit(0)
-    # return result
 
 @timebudget
-def factorize_proc(*number):
-    result = []
-
+def factorize_proc(numbers: List[int]):
+    arr = Manager().list()
     dict_sync = {}
-    for num in number:
-        dict_sync[num] = Process(target=f, args=(num,))
-        dict_sync[num].start()
-    while not all([not res.is_alive() for res in dict_sync.values()]):
+    for number in numbers:
+        dict_sync[number] = Process(target=factorize, args=(number, arr))
+        dict_sync[number].start()
+    while not all([not process.is_alive() for process in dict_sync.values()]):
         """"""
-    return result
+    return arr
 
+
+@timebudget
+def factorize_pool_proc(numbers: List[int]):
+    arr = Manager().list()
+    data = [[num, arr] for num in numbers]
+    Pool(processes=cpu_count()-1).starmap(factorize, data)
+    return arr
 
 
 if __name__ == '__main__':
-    # factorize(128, 255, 99999, 10651060)
-    # print(factorize_sync(10651060, 128, 255, 99999))
-    factorize_proc(128, 255, 99999, 10651060)
+    RESULT = []
+    data = [128, 255, 99999, 1065106000]
+    print(factorize(data))
+    print(factorize_sync(data))
+    print(factorize_proc(data))
+    print(factorize_pool_proc(data))
