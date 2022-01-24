da87c6c8a41515663151c516532889d5aad572a1
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-01-24 14:16:24 +0200

    update factorize

--- module_2/2.5/factorize.py
+++ module_2/2.5/factorize.py
@@ -70,10 +70,10 @@ if __name__ == '__main__':
     functions = [factorize, factorize_sync, factorize_proc, factorize_pool_proc]
     # Checking and displaying the result.
     for func in functions:
-        func_res = func(data)
-        print(func_res)
-        assert func_res[0] == [1, 2, 4, 8, 16, 32, 64, 128]
-        assert func_res[1] == [1, 3, 5, 15, 17, 51, 85, 255]
-        assert func_res[2] == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-        assert func_res[3] == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+        a, b, c, d = func(data)
+        print(a, b, c, d)
+        assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+        assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+        assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+        assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
     
