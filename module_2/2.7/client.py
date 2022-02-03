98e3c38c5a737a969530404f75a184d14419dc67
Olexii Korshunov <korshunov.olexiy@gmail.com>
2022-02-03 08:18:48 +0200

    update 2.7

--- module_2/2.7/client.py
+++ module_2/2.7/client.py
@@ -1,6 +1,7 @@
+# -*- coding: utf8 -*-
 import socket
 from time import sleep
-
+import sys
 
 def simple_client(host: str, port: int, timeout: int):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
@@ -11,7 +12,11 @@ def simple_client(host: str, port: int, timeout: int):
                 socket.timeout(timeout)
                 soc.connect((host, port))
                 soc.setblocking(True)
-                soc.sendall(input('Input data to send: ').encode())
+                # soc.sendall(input('Input data to send: ').encode())
+                msg  = "Тотал Коммандер – удобное и функциональное приложение, которое облегчит работу с файловой системой компьютера. Эта программа настолько проста в пользовании, что считается гениальной! Она еще больше упрощает работу с ПК. Перед вами открываются новые возможности, такие как просмотр скрытых папок, умный поиск внутри файлов, анализ дисков на наличие дубликатов документов, аудиофайлов и прочего и т. д. И все это вы можете получить бесплатно. Настройка Total Commander – это во многом индивидуальный процесс. Наверное, вы как многие любители подобных программ после установки приложения сразу отправляетесь в файл или в меню с конфигурациями, чтобы увидеть, какие есть функции, и что можно изменить. Так вот, Тотал Коммандер понравится вам еще больше, когда вы увидите безграничное количество опций, которые можно настроить внутри программы! А данная статья вам покажет, как это можно сделать?".encode()
+                size_msg = str.encode(str(sys.getsizeof(msg)))
+                soc.sendall(size_msg)
+                soc.sendall(msg)
                 data_recv = soc.recv(1024).decode("utf8")
                 if data_recv.upper() != "OK":
                     print("Couldn't deliver your message. The server may have been shut down.")
--- module_2/2.7/server.py
+++ module_2/2.7/server.py
@@ -1,31 +1,22 @@
-import socket
+# -*- coding: utf8 -*-
 
+import socket
+import sys
 
 def echo_server(host, port):
     with socket.socket() as soc:
         soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         soc.bind((host, port))
         soc.listen()
-        # conn, addr = soc.accept()
-        # print(f"Connected by {addr}")
-        # with conn:
-        #     while True:
-        #         # data = conn.recv(1024)
-        #         data, client = conn.recvfrom(1024)
-        #         if not data:
-        #             break
-        #         print(f'From client: {data.decode("utf8")}')
-        #         # conn.sendto(str.encode("ok"), addr)
-        #         # conn.send(str.encode("ok"))
-        #         soc.sendto(str.encode("ok"), client)
-
-        con, addr = soc.accept()
-        with con:
-            data, client = soc.recvfrom(1024)
-            # if not data:
-            #     break
-            print(f"From {client}: {data.decode('utf8')}")
-            soc.sendto(str.encode("ok"), client)
+        conn, addr = soc.accept()
+        print(f"Connected by {addr}")
+        with conn:
+            print("wait size of data...")
+            size_msg = conn.recv(1024)
+            print(size_msg.decode("utf8"))
+            # data = conn.recv(int(size_msg))
+            # print(f"Size: {sys.getsizeof(data)}, msg: {data.decode('utf8')}")
+            conn.sendall(str.encode("ok"))
 
 
 if __name__ == "__main__":
