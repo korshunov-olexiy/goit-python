fe8ad31011cd34ead7485f8e5bc2f35bca44cff3
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-02-16 08:11:38 +0200

    update 2.8

--- module_2/2.8/generator_data.py
+++ module_2/2.8/generator_data.py
@@ -1,10 +1,12 @@
 from random import randint
 
+
 grades = lambda: randint(1,5)
 subjects = lambda: randint(1,5)
+teachers = lambda: randint(1,3)
 
+print(f"INSERT INTO grades (student_id, teacher_id, subject_id, value) VALUES")
 for student in range(1,31):
     for j in range(0,20):
         subject = subjects()
-        print(f"INSERT INTO students_subjects (students_id, subjects_id) VALUES ({student}, {subject});")
-        print(f"INSERT INTO subjects_grades (subjects_id, grades_id) VALUES ({student}, {grades()});")
+        print(f"({student}, {teachers()}, {subject}, {grades()})", end=", ")
