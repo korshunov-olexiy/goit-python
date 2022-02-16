9fe95afdd5c585a32c7daf5f060e451ac3881205
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-02-16 16:26:26 +0200

    solve #1 in 2.8

--- module_2/2.8/results/1. 5_students.sql
+++ module_2/2.8/results/1. 5_students.sql
@@ -1,4 +1,5 @@
 -- 5 студентов с наибольшим средним баллом по всем предметам.
+-- example: https://site-do.ru/db/sql6.php
 
 select students.last_name as 'last name', students.first_name as 'first name', email, phone, genders.name as sex, AVG(value) as avg_grade
   from grades, students, genders
