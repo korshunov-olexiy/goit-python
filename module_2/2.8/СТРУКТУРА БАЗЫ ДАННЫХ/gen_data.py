b0ba8177b80021789d1a550bd9e606f55f87a190
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-02-18 16:48:19 +0200

    update 2.8

--- module_2/2.8/СТРУКТУРА БАЗЫ ДАННЫХ/gen_data.py
+++ module_2/2.8/СТРУКТУРА БАЗЫ ДАННЫХ/gen_data.py
@@ -15,5 +15,19 @@ departments = load_data("departments.txt", True)
 groups = load_data("groups.txt", False)
 students = load_data("students.txt", False)
 
-for index, department in enumerate(departments):
-    print(department, groups[index], students[index])
+
+sql_departaments = ["INSERT INTO departments (name) VALUES"]
+sql_groups = ["INSERT INTO groups (name, department_id) VALUES"]
+sql_students = ["INSERT INTO students (last_name, first_name, email, phone) VALUES"]
+for index, department in enumerate(departments, start=1):
+    sql_departaments.append(f"(\"{department}\")")
+    for group in groups[index-1]:
+        sql_groups.append(f"(\"{group}\", {index})")
+    for student in students[index-1]:
+        last_name, first_name = student.split()
+        sql_students.append(f"(\"{last_name}\", \"{first_name}\", {index})")
+
+departments = ', '.join(sql_departaments).replace("VALUES,", "VALUES") + ";"
+groups = ', '.join(sql_groups).replace("VALUES,", "VALUES") + ";"
+students = ', '.join(sql_students).replace("VALUES,", "VALUES") + ";"
+print(f"{students}")
