f7d46f236bcea254d895a518e9cb06ddd85cfed4
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-02-18 16:26:49 +0200

    update 2.8

--- module_2/2.8/СТРУКТУРА БАЗЫ ДАННЫХ/gen_data.py
+++ module_2/2.8/СТРУКТУРА БАЗЫ ДАННЫХ/gen_data.py
@@ -1,9 +1,19 @@
-def load_data(f_name: str):
-    with open("students.txt", "r", encoding="utf8") as f:
-        data = [student.split(",") for student in f.read().splitlines()]
-    result = list(map(lambda st: list(map(lambda s: s.split(), st)), data))
+from typing import List
 
-for group in students:
-    for last_name, first_name in group:
-        print(last_name, first_name)
-    print("")
+
+data_for_sql = ["students.txt", "teachers.txt"]
+
+def load_data(f_name: str, is_row: bool = True) -> List:
+    with open(f_name, "r", encoding="utf8") as f:
+        return f.read().splitlines() if is_row else [student.split(",") for student in f.read().splitlines()]
+
+def load_data2(f_name: str) -> List:
+    with open("groups.txt", "r", encoding="utf8") as f:
+        return f.read().strip().split(",")
+
+departments = load_data("departments.txt", True)
+groups = load_data("groups.txt", False)
+students = load_data("students.txt", False)
+
+for index, department in enumerate(departments):
+    print(department, groups[index], students[index])
